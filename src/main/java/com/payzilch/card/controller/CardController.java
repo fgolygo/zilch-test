6c60853b5b1e4f9be0daf7c5f9f1598adad80cfa
filip <filip.golygowski@gmail.com>
2022-01-11 13:48:50 +0100

    Create card

--- src/main/java/com/payzilch/card/controller/CardController.java
+++ src/main/java/com/payzilch/card/controller/CardController.java
@@ -5,10 +5,7 @@ import com.payzilch.card.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
@@ -22,7 +19,7 @@ public class CardController {
     private CardService cardService;
 
     @PostMapping(consumes = "application/json", produces = "application/json")
-    public Mono<ResponseEntity<String>> createTreesAsAuthenticatedUser(@Valid @RequestBody Mono<NewCardDto> card) {
+    public Mono<ResponseEntity<String>> createCard(@Valid @RequestBody Mono<NewCardDto> card) {
         return cardService.create(card);
     }
 
